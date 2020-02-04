b6c5baee6b64946b855b6a20742732175f4da73c
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-03 18:52:02 -0800

    modify app style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Sidebar from './components/Sidebar';
-import Feed from './components/Explore/Feed';
+import Feed from './components/Explore/main/Feed';
+import Suggestion from './components/Explore/right-bar/Suggestion';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <Feed />
-      <div className="suggestion"></div>
+      <Suggestion />
     </div>
   );
 }
